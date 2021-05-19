264596f028a631e4b3624223b0b156647e6744c8
huhanflyingteam <1185438380@qq.com>
2021-05-19 23:26:57 +0800

    HW-Week 11 Commit

---
+++ src/main/java/com/dabing/dao/IProductDao.java
@@ -0,0 +1,31 @@
+package com.dabing.dao;
+
+import com.dabing.model.Product;
+
+import java.io.InputStream;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.List;
+
+
+
+public interface IProductDao {
+
+	public int save(Product product, Connection con) throws SQLException;
+
+	public int delete(Integer productId, Connection con) throws SQLException;
+
+	public int update(Product instance, Connection con) throws SQLException;
+
+	public Product findById(Integer productId, Connection con) throws SQLException;
+
+	public List<Product> findByCategoryId(int categoryId, Connection con) throws SQLException;
+
+	public List<Product> findByPrice(double minPrice, double maxPrice,Connection con) throws SQLException;
+
+	public List<Product> findAll(Connection con) throws SQLException;
+
+	public List<Product> findByProductName(String productName, Connection con) throws SQLException;
+
+	public List<Product> getPicture(Integer productId, Connection con) throws SQLException;
+}
